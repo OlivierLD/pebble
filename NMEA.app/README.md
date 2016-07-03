669d7c37884f81c755aace0da118892a44cce531
OlivierLD <olivier@lediouris.net>
2016-07-03 12:28:22 -0700

    README

--- NMEA.app/README.md
+++ NMEA.app/README.md
@@ -1,9 +1,14 @@
 ## Pebble NMEA app
 ![Start here](screenshot.01.png "Start here")
+Application list
 ![Choose the channel](screenshot.02.png "Choose the channel")
+Press select to start
 ![Channel list](screenshot.03.png "Channel list")
+Srcoll
 ![Hit select](screenshot.04.png "Hit select")
+Choose
 ![Display](screenshot.05.png "Display")
+Displayed!
 
 This app is a WebSocket client for the application described [here](http://www.lediouris.net/RaspberryPI/_Articles/readme.html).
 
--- NMEA.app/app.card.js
+++ NMEA.app/app.card.js
@@ -1,100 +1,44 @@
 /**
  * By OlivSoft
- *
- * For the fonts, see https://developer.pebble.com/blog/2013/07/24/Using-Pebble-System-Fonts/
  */
+
 var UI = require('ui');
-var Vector2 = require('vector2');
 
 var ws = new WebSocket('ws://192.168.1.176:9876');  // TODO Get the URL from a config...
-var selectedChannel;
 
 var main = new UI.Card({
-  title: ' by OlivSoft',
+  title: ' OlivSoft',
   icon: 'images/paperboat.png',
   subtitle: 'NMEA App',
-  body: 'Press SELECT ->\nto start.'
+  body: 'Press SELECT to start.'
 });
 
 main.show();
 var inSelect = false;
 
-var dataWind = new UI.Window({
-  backgroundColor: 'white'
-});
-var topTextfield = new UI.Text({
-  size: new Vector2(140, 60),
-  font: 'gothic-18-bold',
-  color: 'black',
-  textAlign: 'center'
-});
-var dataTextfield = new UI.Text({
-  size: new Vector2(140, 60),
-  font: 'bitham-42-bold',
-  color: 'black',
-  borderColor: 'black',
-  textAlign: 'center'
-});
-var descTextfield = new UI.Text({
-  size: new Vector2(140, 60),
-  font: 'gothic-14',
-  color: 'black',
-  textAlign: 'center',
-  textOverflow: 'fill',
-  position: new Vector2(0, 116),
-});
-var unitTextfield = new UI.Text({
-  size: new Vector2(140, 60),
-  color: 'black',
-  textAlign: 'center',
-  position: new Vector2(0, 130),
-});
-var windSize = dataWind.size();
-// Center the dataTextfield in the window
-var textfieldPos = dataTextfield.position()
-      .addSelf(windSize)
-      .subSelf(dataTextfield.size())
-      .multiplyScalar(0.5);
-dataTextfield.position(textfieldPos);
-dataWind.add(topTextfield);
-dataWind.add(dataTextfield);
-dataWind.add(descTextfield);
-dataWind.add(unitTextfield);
-
-var setData = function(top, value, unit, desc) {
-  topTextfield.text(top);
-  dataTextfield.text(value);
-  descTextfield.text(desc);
-  unitTextfield.text(unit);
-};
-
-var displayData = function(payload) {
-  var card = dataWind; // new UI.Card();
+ws.onmessage = function (event) { 
+  var card = main; // new UI.Card();
   if (card !== undefined) {
-    var display = "No channel selected yet.";
+    var payload = JSON.parse(event.data);
+    var display = "No channel selected";
     if (selectedChannel !== undefined) {
       if (!inSelect) {
         // payload['member'] => payload[channels[index].member].toFixed(channels[index].nbd)
+        card.title('  ' + selectedChannel.chan);
         if (payload[selectedChannel.member]) {
-          display = payload[selectedChannel.member].toFixed(selectedChannel.nbd);
+          display = payload[selectedChannel.member].toFixed(selectedChannel.nbd) +
+                    selectedChannel.unit;
         } else {
-          display = '...';
+          display = 'Not available';
         }
-        setData(selectedChannel.chan,
-                display,
-                selectedChannel.unit,
-                selectedChannel.desc);
-        dataWind.show();
+        card.subtitle(display);
+        card.body(selectedChannel.desc);
+        card.show();
       }
     } else {
       console.log(display);
     }
   }
-};
-
-ws.onmessage = function (event) {
-  var payload = JSON.parse(event.data);
-  displayData(payload);
 };
 
 /*
@@ -105,88 +49,88 @@ var channels = [ { chan: 'BSP',
                    desc: 'Boat Speed',
                    member: 'bsp',
                    nbd: 2,
-                   unit: 'knots' },
-                 { chan: 'HDG',
+                   unit: ' kts' },
+                 { chan: 'HDG', 
                    desc: 'Heading',
                    member: 'hdg',
                    nbd: 0,
-                   unit: 'degrees °' },
+                   unit: '°' },
                  { chan: 'DBT',
                    desc: 'Depth Below Transducer',
                    member: 'dbt',
                    nbd: 2,
-                   unit: 'meters' },
+                   unit: ' m' },                 
                  { chan: 'SOG',
                    desc: 'Speed Over Ground',
                    member: 'sog',
                    nbd: 2,
-                   unit: ' knots' },
+                   unit: ' kts' },                 
                  { chan: 'COG',
                    desc: 'Course Over Ground',
                    member: 'cog',
                    nbd: 0,
-                   unit: 'degrees °' },
-                 { chan: 'AWA',
+                   unit: '°' },                 
+                 { chan: 'AWA', 
                    desc: 'Apparent Wind Angle',
                    member: 'awa',
                    nbd: 0,
-                   unit: 'degrees °' },
-                 { chan: 'AWS',
+                   unit: '°' },
+                 { chan: 'AWS', 
                    desc: 'Apparent Wind Speed',
                    member: 'aws',
                    nbd: 2,
-                   unit: 'knots' },
-                 { chan: 'TWA',
+                   unit: ' kts' },
+                 { chan: 'TWA', 
                    desc: 'True Wind Angle',
                    member: 'twa',
                    nbd: 0,
-                   unit: 'degrees °' },
-                 { chan: 'TWD',
+                   unit: '°' },
+                 { chan: 'TWD', 
                    desc: 'True Wind Direction',
                    member: 'twd',
                    nbd: 0,
-                   unit: 'degrees °' },
-                 { chan: 'TWS',
+                   unit: '°' },
+                 { chan: 'TWS', 
                    desc: 'True Wind Speed',
                    member: 'tws',
                    nbd: 2,
-                   unit: 'knots' },
-                 { chan: 'VMG',
+                   unit: ' kts' },
+                 { chan: 'VMG', 
                    desc: 'Velocity Made Good',
                    member: 'vmg',
                    nbd: 2,
-                   unit: 'knots' },
-                 { chan: 'CSP',
+                   unit: ' kts' },
+                 { chan: 'CSP', 
                    desc: 'Current Speed',
                    member: 'csp',
                    nbd: 2,
-                  unit: 'knots' },
-                 { chan: 'CDR',
+                  unit: ' kts' },
+                 { chan: 'CDR', 
                    desc: 'Current Direction',
                    member: 'cdr',
                    nbd: 0,
-                   unit: 'degrees °' },
-                 { chan: 'CMG',
+                   unit: '°' },
+                 { chan: 'CMG', 
                    desc: 'Course Made Good',
                    member: 'cmg',
                    nbd: 0,
-                   unit: 'degrees °' },
-                 { chan: 'DWP',
+                   unit: '°' },
+                 { chan: 'DWP', 
                    desc: 'Distance to Waypoint',
                    member: 'dwp',
                    nbd: 2,
-                   unit: 'nm' },
-                 { chan: 'PRMSL',
+                   unit: ' nm' },
+                 { chan: 'PRMSL', 
                    desc: 'Pressure at Mean Sea Level',
                    member: 'prmsl',
                    nbd: 1,
-                   unit: 'hPa' },
-                 { chan: 'WTMP',
+                   unit: ' hPa' },
+                 { chan: 'WTMP', 
                    desc: 'Water Temperature',
                    member: 'wtemp',
                    nbd: 1,
                    unit: '°C' },
-                 { chan: 'ATMP',
+                 { chan: 'ATMP', 
                    desc: 'Air Temperature',
                    member: 'atemp',
                    nbd: 1,
@@ -195,7 +139,9 @@ var channels = [ { chan: 'BSP',
                    desc: 'Relative Humidity',
                    member: 'hum',
                    nbd: 1,
-                   unit: '%' }];
+                   unit: ' %' }];
+
+var selectedChannel;
 
 // icon: 'images/paperboat.png',
 
@@ -206,9 +152,9 @@ var menuItems = [{
 
 channels.forEach(function(chan) {
   menuItems.push({title: chan.chan, subtitle: chan.desc });
-});
+});      
 
-var displayMenu = function() {
+main.on('click', 'select', function(e) {
   inSelect = true;
 //console.log('Choosing the data to display');
   var menu = new UI.Menu({
@@ -220,31 +166,14 @@ var displayMenu = function() {
   menu.on('select', function(e) {
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '", desc ' + e.item.subtitle);
-    if (e.itemIndex > 0) {
-      selectedChannel = channels[e.itemIndex - 1];
-      console.log("Selected channel:" + JSON.stringify(selectedChannel));
-      inSelect = false;
-      displayData({}); // Display dummy data
-    }
-  });
-
-  menu.on('back', function(e) { // No such event...
-    menu.hide();
+    selectedChannel = channels[e.itemIndex - 1];
+    console.log("Selected channel:" + JSON.stringify(selectedChannel));
+    inSelect = false;
   });
 //console.log('3 - Showing menu');
   menu.show();
 //console.log('4 - Menu should be visible');
-};
-
-main.on('click', 'select', function(e) {
-  displayMenu();
 });
-
-dataWind.on('click', 'back', function(e) {
-  dataWind.hide();
-  displayMenu();
-});
-
 
 main.on('click', 'up', function(e) {
   console.log('Main UP');
