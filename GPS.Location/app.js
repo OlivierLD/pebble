51fd3bdb35b39490cd9a07da950f93a7e2bb06c8
OlivierLD <olivier@lediouris.net>
2016-07-10 19:30:22 -0700

    Speed

--- GPS.Location/app.js
+++ GPS.Location/app.js
@@ -6,6 +6,9 @@ var Vibe = require('ui/vibe');
 
 var watchId;
 
+var prevFixTime;
+var prevFix;
+
 var main = new UI.Card({
   title: 'Connected',
   icon: 'images/menu_icon.png',
@@ -35,6 +38,27 @@ var lpad = function(s, len, pad) {
   return str;
 };
 
+var degToRadians = function(angle) {
+  return angle * Math.PI / 180.0;
+};
+
+var radiansToDegrees = function(angle) {
+  return angle * 180.0 / Math.PI;
+};
+
+/**
+ * from, to: {lat: 38.85643, lon: -123.98736}, in degrees
+ * Return in nautical miles
+ */
+var getDistance = function(from, to) {
+  var cos = Math.sin(degToRadians(from.lat)) *
+            Math.sin(degToRadians(to.lat)) +
+            Math.cos(degToRadians(from.lat)) *
+            Math.cos(degToRadians(to.lat)) * Math.cos(degToRadians(to.lon) - degToRadians(from.lon));
+  var dist = Math.acos(cos);
+  return radiansToDegrees(dist) * 60; // in nm
+};
+
 var dateHMS = function() {
   var d = new Date();
   return lpad(d.getHours().toString(), 2, '0') + ':' + lpad(d.getMinutes().toString(), 2, '0') + ':' + lpad(d.getSeconds().toString(), 2, '0');
@@ -44,11 +68,22 @@ var dateHMS = function() {
 var onPosSuccess = function(pos) {
   console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
   console.log("Pos data:" + JSON.stringify(pos));
+  var speed;
+  var now = new Date().getTime();
+  if (prevFix !== undefined && prevFixTime !== undefined) {
+    var deltaDist = getDistance(prevFix, { lat:pos.coords.latitude, lon: pos.coords.longitude });
+    speed = (deltaDist * 1852) / ((now - prevFixTime) / 1000); // in m/s
+  }
+  prevFix = { lat:  pos.coords.latitude, lon: pos.coords.longitude };
+  prevFixTime = now;
   if (main !== undefined) {
     var card = main; // new UI.Card();
     card.title('Location');
     card.subtitle('');
-    card.body(formatPos(pos.coords.latitude, 'L') + '\n' + formatPos(pos.coords.longitude, 'G') + '\nRefreshed at ' + dateHMS());
+    card.body(formatPos(pos.coords.latitude, 'L') + '\n' +
+              formatPos(pos.coords.longitude, 'G') + '\n' +
+              'At ' + dateHMS() + (speed !== undefined ? '\n' +
+                                             'Spd:' + (speed * 3.6).toFixed(speed < 10 ? 2 : (speed < 100? 1: 0)) + ' km/h' : ''));
     card.show();
   }
   if (watchId !== undefined) {
