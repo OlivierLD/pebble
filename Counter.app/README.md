4696a26ea893eb6318276211aa756129562e92de
OlivierLD <olivier@lediouris.net>
2016-09-03 09:47:35 -0700

    Counter with doc

---
+++ Counter.app/README.md
@@ -0,0 +1,24 @@
+## Pebble Sheep Counter app
+
+Copy the content of `app.js` in your [CloudPebble](https://cloudpebble.net/ide/) account.
+
+<table>
+  <tr>
+    <td align="center" valign="top">
+      Welcome screen, <br/>use up, down, reset
+    </td>
+    <td align="center" valign="top">
+      Counting
+    </td>
+  </tr>
+  <tr>
+    <td align="center" valign="top">
+      <img src="01.png" alt="Start here">
+    </td>
+    <td align="center" valign="top">
+      <img src="02.png" alt="Counting">
+    </td>
+  </tr>
+</table>
+
+Use `Back` to return to the welcome screen, and `Back` again to exit.
--- Counter.app/app.js
+++ Counter.app/app.js
@@ -1,41 +1,73 @@
 /**
- * A simple counter.
+ * A simple sheep counter.
  * Up     +1
  * Down   -1
  * Select Reset
  */
 var UI = require('ui');
+var Vector2 = require('vector2');
 
 var counterValue = 0;
 
 var main = new UI.Card({
-  title: ' ' + counterValue.toFixed(0),
+  title: ' Counter',
   icon: 'images/menu_icon.png',
-  subtitle: 'Counter',
-  body: 'Up:+, Down:-, Select:Reset'
+  subtitle: 'Operation:',
+  body: 'Up:+ Down:- Select:Reset'
 });
 
 main.show();
 
+// New data window for the counter value
+var dataWind = new UI.Window({
+  backgroundColor: 'white'
+});
+var dataTextfield = new UI.Text({
+  size: new Vector2(140, 60),
+  font: 'bitham-42-bold',
+  color: 'black',
+  borderColor: 'black',
+  textAlign: 'center'
+});
+var windSize = dataWind.size();
+var textfieldPos = dataTextfield.position()
+      .addSelf(windSize)
+      .subSelf(dataTextfield.size())
+      .multiplyScalar(0.5);
+dataTextfield.position(textfieldPos);
+dataWind.add(dataTextfield);
+
+var setData = function(value) {
+  dataTextfield.text(value);
+};
+
 var displayValue = function() {
-  if (main !== undefined) {
-    var card = main; // new UI.Card();
-    card.title(' ' + counterValue.toFixed(0));
+  if (dataWind !== undefined) {
+    var card = dataWind;
+    setData(counterValue);
     card.show();
   }
 };
 
-main.on('click', 'select', function(e) {
+var select = function(e) {
   counterValue = 0;
   displayValue();
-});
+};
 
-main.on('click', 'up', function(e) {
+main.on('click', 'select', select);
+dataWind.on('click', 'select', select);
+
+var up = function(e) {
   counterValue += 1;
   displayValue();
-});
+};
+main.on('click', 'up', up);
+dataWind.on('click', 'up', up);
 
-main.on('click', 'down', function(e) {
+var down = function(e) {
   counterValue -= 1;
   displayValue();
-});
+};
+
+main.on('click', 'down', down);
+dataWind.on('click', 'down', down);
