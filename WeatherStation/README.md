70ae857c28004b76103e5161b97a2a18af5e9982
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-03-18 09:15:44 -0700

    Weather Station app

--- WeatherStation/README.md
+++ WeatherStation/README.md
@@ -1,4 +1,8 @@
 ## Pebble REST client, used to reach an weather data
 
 Sample REST client application featuring
-- ...
+- One landing page
+- One data page
+
+![One](./screen.01.png) ![One](./screen.02.png)
+
--- WeatherStation/app.js
+++ WeatherStation/app.js
@@ -1,6 +1,25 @@
 /**
  * Interaction with a specific REST URL
  * GET Weather Data
+ *
+ * Sample weather data:
+ * {
+ *   "type":"ALL",
+ *   "period":"LAST",
+ *   "query":"SELECT log_time, dew, FORMAT(wdir, 0) as wdir, FORMAT(wgust, 2) as wgust, FORMAT(wspeed, 2) as wspeed, rain, press / 100 as press, atemp, hum FROM weather_data WHERE log_time = (SELECT MAX(log_time) FROM weather_data) ORDER BY log_time ASC ",
+ *   "data":[
+ *     { "time": "2018-03-18 15:21:17",
+ *       "wdir": 71,
+ *       "gust":4.93,
+ *       "ws":4.37,
+ *       "rain":0.000,
+ *       "press":1020.1100000,
+ *       "atemp":10.200,
+ *       "hum":70.057,
+ *       "dew":5.000
+ *     }
+ *   ]
+ * }
  */
 
 var UI = require('ui');
@@ -12,16 +31,47 @@ var URL = "http://donpedro.lediouris.net/php/weather/reports.v2/json.data.php?ty
 var main = new UI.Card({
 	title: ' Click!',
 	icon: 'images/cloud.png',
-	body: 'Up or Down: Refresh',
+	body: 'Select to Refresh',
 	subtitleColor: 'indigo', // Named colors
 	bodyColor: '#9a0036', // Hex colors
 	action: {
-		up: 'images/action_bar_icon_check.png',
-		down: 'images/action_bar_icon_dismiss.png'
+		select: 'images/music_icon_play.png'
 	}
 });
 
 main.show();
+
+// New data window for the weather data
+var dataCard = new UI.Card({
+	backgroundColor: 'white',
+	body: 'Placeholder'
+});
+
+/**
+ * Sample data:
+ *     { "time": "2018-03-18 15:21:17",
+ *       "wdir": 71,
+ *       "gust":4.93,
+ *       "ws":4.37,
+ *       "rain":0.000,
+ *       "press":1020.1100000,
+ *       "atemp":10.200,
+ *       "hum":70.057,
+ *       "dew":5.000 }
+ *
+ * @param data
+ */
+var setData = function(data) {
+	var dataString =
+			'At ' + data.time.split(' ')[1] + ' UT\n' +
+			'W:' + data.ws + 'kts, ' + data.wdir + '°\n' +
+	    'T:' + data.atemp.toFixed(1) + '°C,D:' + data.dew.toFixed(1) + '°C\n' +
+			'Pr:' + data.press.toFixed(1) + ' hPa\n' +
+			'Hum:' + data.hum.toFixed(1) + '%\n' +
+			'Rain:' + data.rain.toFixed(2) + 'mm/h';
+//console.log('Composed:' + dataString);
+	return dataString;
+};
 
 var downloadData = function() {
 	var url = URL;
@@ -29,13 +79,19 @@ var downloadData = function() {
 
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4 && xhr.status === 200) {
-			console.log("XHR returns", xhr.responseText);
+//		console.log("XHR returns", xhr.responseText);
 			if (main !== undefined) {
 				var card = main; // new UI.Card();
 				var resp = JSON.parse(xhr.responseText);
 				if (resp !== undefined && resp.data !== undefined && resp.data.length > 0) {
-					card.body(JSON.stringify(resp.data[0])); // TODO a better UI ;)
-					card.show();
+					if (dataCard !== undefined) {
+						var weatherDataCard = dataCard; // new UI.Card();
+						weatherDataCard.body(setData(resp.data[0]));
+						weatherDataCard.show();
+					} else {
+						card.body(JSON.stringify(resp.data[0])); // raw json
+						card.show();
+					}
 				} else {
 					console.log(JSON.stringify(resp, null, 2));
 				}
@@ -47,17 +103,17 @@ var downloadData = function() {
 	xhr.open("GET", url, true);
 	xhr.setRequestHeader("Content-type", "application/json");
 	xhr.send();
-	console.log("Weather was requested");
+	console.log("Weather data was requested");
 };
 
 main.on('click', 'select', function(e) {
-	console.log("Do nothing");
+	downloadData();
 });
 
 main.on('click', 'up', function(e) {
-	downloadData();
+	console.log("Do nothing");
 });
 
 main.on('click', 'down', function(e) {
-	downloadData();
+	console.log("Do nothing");
 });
